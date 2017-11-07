20f16d4fe33dfd5b1cb5b0bd611cebde5a4601aa
Ronnie Scott <rscott910@outlook.com>
2017-11-07 21:11:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,12 @@ function sayHiToGrandma(string) {
   } else { if (var = uppercase) {
     console.log("YES INDEED!")
   }
-
   }
 }
+var uppercase = 'HELLO!'
+uppercase.toUpperCase() === uppercase
+var lowercase = 'hello!'
+lowercase.toLowerCase() === lowercase
+var mixedcase = 'Hi there!'
+mixedcase.toLowerCase() === mixedcase
+mixedcase.toUpperCase() === mixedcase
