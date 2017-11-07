ceeca19db550628a07e33e7863f830b93c78dd5a
Ronnie Scott <rscott910@outlook.com>
2017-11-07 21:49:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,13 @@ function logShout(string) {
 function logWhisper(string) {
   console.log("hello")
 }
+function sayHiToGrandma(string){
+   if (string === string.toLowerCase()){
+     return 'I can\'t hear you!';
+   } else if (string === string.toUpperCase()){
+     return 'YES INDEED!';
+   } else if ('I love you, Grandma.') {
+     return 'I love you, too.'
+   }
+ }
+ 
